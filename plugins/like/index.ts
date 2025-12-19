101bb8d050a4a5001514420288a7885cbc7c7b09
Viki <hi@viki.moe>
2025-12-19 21:42:17 +0800

    chore: 点赞

---
+++ plugins/like/index.ts
@@ -0,0 +1,14 @@
+import { definePlugin } from 'mioki'
+
+export default definePlugin({
+  name: '赞我',
+  version: '1.0.0',
+  async setup(ctx) {
+    ctx.handle('message.group', async (event) => {
+      if (event.raw_message === '赞我') {
+        await ctx.bot.sendLike(event.sender.user_id, 10)
+        await event.addReaction('66')
+      }
+    })
+  },
+})
