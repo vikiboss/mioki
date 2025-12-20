ce66d0585942e559312de0e9b01fe443afc66777
Viki <hi@viki.moe>
2025-12-20 22:39:30 +0800

    chore: 优化插件

--- plugins/like/index.ts
+++ plugins/like/index.ts
@@ -6,8 +6,8 @@ export default definePlugin({
   async setup(ctx) {
     ctx.handle('message.group', async (event) => {
       if (event.raw_message === '赞我') {
-        await ctx.bot.sendLike(event.sender.user_id, 10)
-        await event.addReaction('66')
+        const isOK = await ctx.bot.sendLike(event.sender.user_id, 10)
+        await event.addReaction(isOK ? '66' : '67')
       }
     })
   },
